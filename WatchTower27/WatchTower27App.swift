5d6689bcae1694e8cef3d9a2430d2cefe03c398c
Nikoloz Gvelesiani <nikolozgvelesiani10@gmail.com>
2024-08-03 21:07:05 +0400

    Initial Commit

---
+++ WatchTower27/WatchTower27App.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTower27App.swift
+//  WatchTower27
+//
+//  Created by Nikoloz Gvelesiani on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchTower27App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
